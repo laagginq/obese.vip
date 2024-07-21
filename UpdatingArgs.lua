3fe4ca0b854892d397290895c3b5e44b85b0bdd9
xz <95953670+laagginq@users.noreply.github.com>
2024-07-21 12:27:13 -0700

    Create UpdatingArgs.lua

---
+++ UpdatingArgs.lua
@@ -0,0 +1,28 @@
+local Args = {
+    ["UpdateMousePosI"] = {
+        2788229376, -- // DH
+        7213786345, -- // DHVC
+        16033173781, -- // DHMACRO
+    },
+    ["MousePos"] = {
+        5602055394, -- // HM
+        7951883376, -- // HMVC
+    },
+    ["MOUSE"] = {
+        17403265390,
+        17403166075,
+        17403262882,
+        15186202290,
+        15763494605,
+    },
+    ["UpdateMousePos"] = {
+        17319408836, -- // OGDH
+        11143225577, -- // 1v1 AimTrainer
+		17502123056, -- // AR hood
+    },
+    ["MoonUpdateMousePos"] = {
+        15166543806, -- // HM
+    },
+}
+-- // If u find this feel free to use on ur script js credit me (xz @swipingfraud2)
+return Args
