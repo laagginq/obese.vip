4e52ed3ce05399a9100b3737bd9caa68fbeb5ddf
xz <95953670+laagginq@users.noreply.github.com>
2024-07-21 20:52:13 -0700

    Update UpdatingArgs.lua

--- UpdatingArgs.lua
+++ UpdatingArgs.lua
@@ -1,27 +1,33 @@
 return {
     ["UpdateMousePosI"] = {
-        2788229376, -- // DH
-        7213786345, -- // DHVC
-        16033173781, -- // DHMACRO
+        2788229376, -- // Da Hood
+        7213786345, -- // Da Hood VC
+        16033173781, -- // Da Hood Macro
     },
     ["MousePos"] = {
-        5602055394, -- // HM
-        7951883376, -- // HMVC
+        5602055394, -- // Hood Modded
+        7951883376, -- // Hood Modded VC
     },
     ["MOUSE"] = {
-        17403265390,
-        17403166075,
-        17403262882,
-        15186202290,
-        15763494605,
+        17403265390, -- // Da Downhill
+        15186202290, -- // Da Strike
+	18111448661, -- // Da Uphill
+	18111451594, -- // Da Bank
+	16560956150, -- // Da Office
+	18198680772, -- // Da Concrete Jungle
+	17564718305, -- // Da Nuketown
+	17564716597, -- // Da Crossroads
     },
     ["UpdateMousePos"] = {
         17319408836, -- // OGDH
         11143225577, -- // 1v1 AimTrainer
 	17502123056, -- // AR hood
+	14174018683, -- // JS Hood
+	18177028900, -- // Wanted Hood
+	18459606288, -- // Fatal Hood
     },
     ["MoonUpdateMousePos"] = {
-        15166543806, -- // HM
+        15166543806, -- // Moon Hood
     }
 }
 -- // If u find this feel free to use on ur script js credit me (xz @swipingfraud2)
